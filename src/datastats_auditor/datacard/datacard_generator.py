e5806bc58f58f43a3fe7d2c612983225a9e6ac22
linus <lagbleze@st.ug.edu.gh>
2026-03-11 22:57:47 +0100

    feat: add create_data_card

--- src/datastats_auditor/datacard/datacard_generator.py
+++ src/datastats_auditor/datacard/datacard_generator.py
@@ -5,6 +5,25 @@ from pathlib import Path
 import os
 from markdown_pdf import MarkdownPdf, Section
 import base64
+from datastats_auditor.datacard.card_sections import (create_section, 
+                                                      create_drift_section, 
+                                                      create_scene_composition_section,
+                                                      generate_data_metric_section,
+                                                      create_drift_section,
+                                                      create_section_head
+                                                      )
+
+from datastats_auditor.datacard.utils import (compute_summary_stats_wider, 
+                                              concat_split_dfs,
+                                              write_md_to_pdf,
+                                              create_data_overview
+                                              )
+from datastats_auditor.datacard.visualizer import (plot_summary_table,
+                                                   plot_bar, HistPlot
+                                                   )
+from datastats_auditor.datacard.constants import (PDF_PATH)
+
+
 
 class DatasetCardCreator:
     def __init__(self, sections: dict, renderer):
@@ -81,28 +100,6 @@ def build_motivations_and_use(
     return section.strip()        
 
 
-def fig_to_base64(fig):
-    img_bytes = fig.to_image(format="png")
-    return base64.b64encode(img_bytes).decode("utf-8")
-    
-    
-def generate_data_metric_section(metric_heading, fig,
-                                 subheading="", footnote="",
-                                 ):
-    img_b64 = fig_to_base64(fig)
-    img_md = f"![splits](data:image/png;base64,{img_b64})"
-
-    md_content = f"""
-    ## {metric_heading}
-    
-    ### {subheading}
-
-    {img_md}
- 
-    {footnote}
-    """
-    return md_content
-    
     
     
 
@@ -125,22 +122,111 @@ class MarkdownRenderer:
         return md
 
 
-def create_section(*args, **kwargs):
-    kwargs = kwargs.get("kwargs")
-    header = kwargs.get("header", "")
-    header = f"{header}" if header else ""
-    sect_content = "\n".join([f"- {key}: {value}" for key, value in kwargs.items() if key != "header"])
-    #sect_content = "\n".join(sect_content)
-    # font-size:20px;
-    section = f""" 
-<H1 style="color:#2A7FFF; font-weight:bold;">
- {header}
-</H1>
 
-{sect_content}
+
+def create_data_card(split_stats_result,
+                     drift_result,
+                     version_id, name,
+                     intended_objects=None,
+                     pdf_path=PDF_PATH,
+                     **kwargs
+                    ):
+    summary_stats_df = compute_summary_stats_wider(split_stats_result)
+    wider_summary_table_fig = plot_summary_table(summary_stats_df, height=300)
+    full_split_df = concat_split_dfs(split_dfs=split_stats_result["split_dfs"])
+
+    object_dist_barplot = plot_bar(full_split_df.groupby(["split_type", "category_name"]).size().reset_index(name="count"),
+                                        x="category_name", y="count",
+                                        title="Category Distribution by Split",
+                                        facet_row="split_type",
+                                        color="split_type",
+                                        color_discrete_sequence=px.colors.qualitative.Plotly,
+                                        labels={"category_name": "Category", "count": "Count", "split_type": "Split"},
+                                        text="count",
+                                        height=500, width=700,
+                                        
+                                        )
+        
+        
+    drift_section = create_drift_section(drift_result=drift_result)
+
+
+    histplot_cls = HistPlot(df=full_split_df, 
+                                property_names=["relative_bbox_area", 'bbox_aspect_ratio',
+                                                'occupancy_per_image', 'num_bboxes_per_image',
+                                                ],
+                            facet_row="split_type",
+                            height=500, width=700,
+                            )
+    hist_figs = histplot_cls.create_histograms()
+
+
+    scene_composition_section = create_scene_composition_section(hist_figs)
+        
+    drift_scene_section = "\n".join([i for i in [drift_section, scene_composition_section]])
+
+    intended_objects = list(full_split_df["category_name"].unique()) if intended_objects is None else intended_objects 
+    moti_content = build_motivations_and_use(intended_tasks=kwargs.get("intended_tasks", intended_tasks),
+                                            intended_objects=intended_objects, #["Person", "Helmet", "Safety vest"],
+                                            )
+    object_bias_content = generate_data_metric_section(metric_heading=f"Object Balance per Split",
+                                                        fig=object_dist_barplot,
+                                                        )
+
+
+
+    summary_kwargs = create_data_overview(split_dfs=split_stats_result["split_dfs"], 
+                                            name=name, id=version_id
+                                            )
+
+
+    summary_section = create_section(kwargs=summary_kwargs)
+    authorship_section = create_section(kwargs=auth_kwargs)
+    data_collection_section = create_section(kwargs=data_col_kwargs)
+    labelling_section = create_section(kwargs=labelling_kwargs)
+    split_section = create_section(kwargs=split_kwargs)
+    transformation_section = create_section(kwargs=trans_kwargs)
+    license_section = create_section(kwargs=licence_kwargs)
+
+
+    wider_summary_table_fig_base64 = generate_data_metric_section(metric_heading="",
+                                                                    fig=wider_summary_table_fig
+                                                                    )
+
+
+
+    title = create_section_head(header="Data Card")
+    data_overview = create_section_head(header="Data Overview",
+                                        section=summary_section
+                                        )
+    wider_summary_statistics_section = create_section_head(header="Descriptive Summary Statistics",
+                                                            section=wider_summary_table_fig_base64
+                                                            )
+    dataspace_metrics = create_section_head(header="Data Space Metrics",
+                                            section=drift_scene_section, #'drift_contents
+                                            )
+    motivation_section = create_section_head(header="Motivation and Intended Use",
+                                            section=moti_content
+                                            )
+    joined_section = "\n".join([title, 
+                                authorship_section, 
+                                motivation_section, 
+                                data_overview,
+                                data_collection_section, 
+                                labelling_section, 
+                                split_section,
+                                wider_summary_statistics_section, 
+                                object_bias_content,
+                                dataspace_metrics, 
+                                transformation_section,
+                                license_section
+                                ])
+
+    write_md_to_pdf(joined_section, pdf_path=pdf_path)
+
+
     
-    """
-    return section.strip()
+    
         
 #%%
 if __name__ == "__main__":
