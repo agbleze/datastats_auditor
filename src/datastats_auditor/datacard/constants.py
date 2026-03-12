4fa5aeb961e01563c37be15d61a61b350f07893f
linus <lagbleze@st.ug.edu.gh>
2026-03-12 20:01:49 +0100

    feat: add constant module

---
+++ src/datastats_auditor/datacard/constants.py
@@ -0,0 +1,41 @@
+
+
+
+PDF_PATH = "datacard.pdf"
+
+AUTHORSHIP = {"header": "Authorship & Ownership",
+            "Organization": "ORG",
+            "Industry": "TECH",
+            "Dataset owners": "AI TEAM"                   
+            }
+
+DATA_COLLECTION = {"header": "Data Collection",
+                    "Collection strategy": "camera recording",
+                    "Devices": "mobile phone",
+                    "Collection Sites": "farm",
+                    "Environmental conditions": "indoor/outdoor, lighting, weather"
+                    }
+
+
+LABELLING = {"header": "Annotation & Labeling",
+                    "Labeling method": "human",
+                    "Label types": "bbox, segmentation mask",
+                    "Annotation format": "COCO",
+                    "Annotation review method": "(SME, same labelers, etc.)",
+                    "Platform": "CVAT"    
+                    }
+
+LICENCE = {"header": "Licensing & Usage",
+                "License": "BSD",
+                "Usage restrictions": "Internal-only",
+                "Redistribution policy": "N/A"
+                }
+
+TRANSFORMATION = {"header":"Transformation",
+                "Technique":"Augmentation",
+                "Parameters": {},
+                "Libraries used": "Augment" 
+                }
+
+
+
