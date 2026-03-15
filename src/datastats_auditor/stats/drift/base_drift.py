ee003c9e2d2a36b387daf76e37fc5db9df9f65ab
linus <lagbleze@st.ug.edu.gh>
2026-03-15 19:33:56 +0100

    feat: add BaseDrift

---
+++ src/datastats_auditor/stats/drift/base_drift.py
@@ -0,0 +1,27 @@
+from abc import ABC, abstractmethod
+from typing import Literal
+from ..registry import registry
+
+
+class BaseDrift(ABC):
+    name: str = "base_drift"
+    status: Literal["stable", "experimental"] = "stable"
+    required = ["name", "status"]
+    valid_status_values = ["stable", "experimental"]
+    
+    def __init_subclass__(cls):
+        super().__init_subclass__()
+        for attr in cls.required:
+            if attr not in cls.__dict__:
+                raise NotImplementedError(f"{cls.__name__} requires definining a '{attr}' class attribute because it is a Subclass of BaseDrift.")
+        if cls.status not in cls.valid_status_values:
+            raise ValueError(f"{cls.__name__} has invalid status '{cls.status}'. Valid values are {cls.valid_status_values}.")
+
+        registry.register(cls.name, cls, cls.status)
+        
+    @abstractmethod
+    def compute_drift(self, *args, **kwargs):
+        raise NotImplementedError("Subclasses must implement the compute_drift method.")
+    
+    
+    
