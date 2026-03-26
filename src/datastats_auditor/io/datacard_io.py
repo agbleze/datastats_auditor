c74f8473d893d4b3a1c8fcd3558db6c69b9d606d
linus <lagbleze@st.ug.edu.gh>
2026-03-26 21:16:32 +0100

    feat: add io

---
+++ src/datastats_auditor/io/datacard_io.py
@@ -0,0 +1,18 @@
+from ..stats.datacard.core.io.baseio import BaseCardExporter
+from ..stats.datacard.utils import write_md_to_pdf
+
+
+class LocalCardExporter(BaseCardExporter):
+    name = "local_card_exporter"
+    status = "stable"
+    
+    def __init__(self, markdown_content, pdf_path, **kwargs):
+        self.markdown_content = markdown_content
+        self.pdf_path = pdf_path
+        
+    def export(self):
+        write_md_to_pdf(markdown_content=self.markdown_content,
+                        pdf_path=self.pdf_path
+                        )
+        
+        
---
+++ src/datastats_auditor/io/local_io.py
@@ -0,0 +1,47 @@
+from pathlib import Path
+from datastats_auditor.stats.io.baseio import IterableDataset
+from datastats_auditor.stats.constant import VALID_IMAGE_EXTENSIONS
+import numpy as np
+from PIL import Image
+
+
+
+class ImageBatchDataset(IterableDataset):
+    name = "local_image_loader"
+    status = "stable"
+    
+    def __init__(self, image_dir, batch_size=10, normalize=True):
+        self.root = Path(image_dir)
+        self.paths = [p for p in self.root.glob("*") if p.suffix.lower() in VALID_IMAGE_EXTENSIONS]
+        self.num_samples = len(self.paths)
+        self.batch_size = batch_size
+        self.normalize = normalize
+        
+    def __len__(self):
+        return self.num_samples
+    
+    def __iter__(self):
+        for i in range(0, self.num_samples, self.batch_size):
+            batch_paths = self.paths[i:i+self.batch_size]
+            batch = []
+            for path in batch_paths:
+                try:
+                    img = np.array(Image.open(path), dtype=np.float32)
+                except Exception as e:
+                    raise RuntimeError(f"Error loading image {path}: {e}")
+                if self.normalize:
+                    img = img / 255.0
+                if img.ndim == 2:
+                    img = img[..., None] # grayscale to (H,W,1)
+                img = np.transpose(img, (2,0,1)) # HWC -> CHW
+                batch.append(img)
+            yield np.stack(batch, axis=0) # (B,C,H,W)
+            
+    def __getitem__(self, idx):
+        return self.paths[idx]
+        
+
+
+class LocalStatsResultExporter():
+    def __init__(self):
+        pass
