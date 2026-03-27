a13c38054e2efff1e7a6b195ec3410a86bca2a42
linus <lagbleze@st.ug.edu.gh>
2026-03-27 22:08:04 +0100

    style: remove comments

--- src/datastats_auditor/engine/orchestrator.py
+++ src/datastats_auditor/engine/orchestrator.py
@@ -5,8 +5,10 @@ from ..stats.split_service.split_stat_service import SplitStatsComputerService
 from ..stats.split_service.base_split_service import BaseSplitStatsComputerService
 from ..stats.drift.base_drift_service import BaseDriftComputerService
 from ..stats.drift.base_drift import BaseDrift
-from ..datacard.datacard_generator import create_data_card
-from ..datacard.constants import PDF_PATH, METRICS, FIELD_TO_BIN
+from ..stats.datacard.datacard_generator import create_data_card
+from ..stats.datacard.constants import PDF_PATH, METRICS, FIELD_TO_BIN
+from ..stats.datacard.base_card_creator import BaseCardCreator
+from ..stats.datacard.core.io.baseio import BaseCardExporter
 
 
 
@@ -22,15 +24,12 @@ def concat_split_dfs(split_dfs: dict):
 
 def compute_stats_and_drift(split_stats_service: BaseSplitStatsComputerService,
                             drift_stats_service: BaseDriftComputerService,
-                            drift_cls: BaseDrift,
+                            drift_computer: BaseDrift,
+                            card_creator: BaseCardCreator,
                             metrics: List=METRICS, 
                             field_to_bin: List=FIELD_TO_BIN,
-                            # compute_spatial_drift=True,
-                            # x_coordinate_field="relative_x_center",
-                            # y_coordinate_field="relative_y_center",
-                            # spatial_strategy="equal",
-                            # spatial_n_bins=10,
                             make_date_card: bool = True,
+                            card_exporter: BaseCardExporter = None,
                             **kwargs
                             ):
       
@@ -39,21 +38,25 @@ def compute_stats_and_drift(split_stats_service: BaseSplitStatsComputerService,
     distributions = split_stats_res.split_dfs    
                 
     drift = drift_stats_service(distributions=distributions,
-                                drift_cls=drift_cls,
+                                drift_cls=drift_computer,
                                 metrics=metrics,
-                                field_to_bin=field_to_bin
+                                field_to_bin=field_to_bin,
+                                **kwargs
                                 )
     
     drift_results = drift.compute_drift_metrics()
     
     if make_date_card:
-        create_data_card(split_stats_result=split_stats_res,
-                        drift_result=drift_results,
-                        version_id=kwargs.get("version_id", str(uuid.uuid1())),
-                        name=kwargs.get("name", "demo"),
-                        intended_objects=kwargs.get("intended_objects"),
-                        pdf_path=kwargs.get("pdf_path", PDF_PATH)
-                        )
+        card_creator = card_creator(split_stats_result=split_stats_res,
+                                    drift_result=drift_results,
+                                    **kwargs
+                                    )
+        card_content = card_creator.create_card()
+        card_exporter = card_exporter(card_content, 
+                                      **kwargs
+                                    )
+        card_exporter.export()
+        
     return {"split_stats_result": split_stats_res,
             "drift_results": drift_results
             }
@@ -61,18 +64,3 @@ def compute_stats_and_drift(split_stats_service: BaseSplitStatsComputerService,
 
 
 
-#%%
-
-"""computer
-            - name
-                objectstate
-                    params: 
-                        ---
-                imagestat:
-                    params:
-                        ---
-    
-    """
-    
-    
-    
