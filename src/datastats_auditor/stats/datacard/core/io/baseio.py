55c7a422ad8032c052c934efb9f3f1cc5c8b32f0
linus <lagbleze@st.ug.edu.gh>
2026-03-28 21:33:03 +0100

    style: remove unused imports and space

---
+++ src/datastats_auditor/stats/datacard/core/io/baseio.py
@@ -0,0 +1,23 @@
+from abc import ABC, abstractmethod
+from typing import Literal
+from ....registry import registry
+
+class BaseCardExporter(ABC):
+    name: str = "base_card_exporter"
+    status: Literal["stable", "experimental"] = "stable"
+    required = ["name", "status"]
+    valid_status_values = ["stable", "experimental"]
+    
+    def __init_subclass__(cls):
+        super().__init_subclass__()
+        for attr in cls.required:
+            if attr not in cls.__dict__:
+                raise NotImplementedError(f"{cls.__name__} requires definining a '{attr}' class attribute because it is a Subclass of BaseCardExporter.")
+        if cls.status not in cls.valid_status_values:
+            raise ValueError(f"{cls.__name__} has invalid status '{cls.status}'. Valid values are {cls.valid_status_values}.")
+
+        registry.register(cls.name, cls, cls.status)
+        
+    @abstractmethod
+    def export(self, *args, **kwargs):
+        raise NotImplementedError("Subclasses must implement the export method.")
