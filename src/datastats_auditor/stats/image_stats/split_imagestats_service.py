6eb731a550f393ffa1c430f00fd75f36efdd685a
linus <lagbleze@st.ug.edu.gh>
2026-03-29 23:50:35 +0200

    style: remove space

--- src/datastats_auditor/stats/image_stats/split_imagestats_service.py
+++ src/datastats_auditor/stats/image_stats/split_imagestats_service.py
@@ -1,7 +1,6 @@
 from .base_imagestat_service import BaseImageStatsService
 from .base_imagestats import BaseImageStats
 from ..io.baseio import IterableDataset
-from typing import Literal
 from types import SimpleNamespace
 from ..entities import SplitStatsResult
 import os
@@ -15,7 +14,6 @@ class SplitImageStatsComputerService(BaseImageStatsService):
     
     def __init__(self, imagestat_computer: BaseImageStats, 
                  dataloader: IterableDataset,
-                 #dataloader_params,
                  **kwargs
                  ):
         self.image_stats_cls = imagestat_computer
@@ -26,14 +24,13 @@ class SplitImageStatsComputerService(BaseImageStatsService):
                                     }
         if not self.dataloader_params:
             raise ValueError(f"Image directory was not provided")
-        #self.dataloader_params = dataloader_params
     
     def compute_split_image_stats(self): 
         for split_nm, split_param in self.dataloader_params.items():
             print(f"Computing image stats for {split_nm} split...")
             dataloader = self.dataloader_cls(**split_param)
             image_stat = self.image_stats_cls(dataloader)
-            imagestat_results = image_stat.compute_image_stats() #compute_dataset_stats(dataset)
+            imagestat_results = image_stat.compute_image_stats() 
             setattr(SplitImageStatsResult, split_nm, imagestat_results)
             print(f"Finished computing image stats for {split_nm} split.")
         return SplitStatsResult(image_stats=SplitImageStatsResult)
