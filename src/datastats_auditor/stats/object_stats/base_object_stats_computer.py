fcf1054d9dbf7ee052958f15ac45c9b3bb162517
linus <lagbleze@st.ug.edu.gh>
2026-03-30 00:07:43 +0200

    style: change error message

--- src/datastats_auditor/stats/object_stats/base_object_stats_computer.py
+++ src/datastats_auditor/stats/object_stats/base_object_stats_computer.py
@@ -1,7 +1,5 @@
-from typing import List, Optional
-import numpy as np
+from typing import List, Literal
 from abc import ABC, abstractmethod
-from typing import Literal
 from ..registry import registry
 
 
@@ -23,6 +21,4 @@ class BaseObjectStatsComputer(ABC):
         
     @abstractmethod
     def compute_object_stats(self, *args, **kwargs) -> List[float]:
-        raise NotImplementedError("Subclasses must implement the compute_image_stats method.")
-    
-    
+        raise NotImplementedError("Subclasses must implement the compute_object_stats method.")
