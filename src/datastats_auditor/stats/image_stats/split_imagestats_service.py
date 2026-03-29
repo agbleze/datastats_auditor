601b5a6d05cb53c5adaa00b66afe2b1eb223146c
linus <lagbleze@st.ug.edu.gh>
2026-03-29 23:53:26 +0200

    fix: add logger

--- src/datastats_auditor/stats/image_stats/split_imagestats_service.py
+++ src/datastats_auditor/stats/image_stats/split_imagestats_service.py
@@ -3,6 +3,7 @@ from .base_imagestats import BaseImageStats
 from ..io.baseio import IterableDataset
 from types import SimpleNamespace
 from ..entities import SplitStatsResult
+from datastats_auditor import logger
 import os
 
 
@@ -21,16 +22,16 @@ class SplitImageStatsComputerService(BaseImageStatsService):
         
         self.dataloader_params = {k: v for k, v in kwargs.items() 
                                     if isinstance(v, dict) and os.path.isdir(list(v.values())[0])
-                                    }
+                                }
         if not self.dataloader_params:
             raise ValueError(f"Image directory was not provided")
     
     def compute_split_image_stats(self): 
         for split_nm, split_param in self.dataloader_params.items():
-            print(f"Computing image stats for {split_nm} split...")
+            logger.info(f"Computing image stats for {split_nm} split...")
             dataloader = self.dataloader_cls(**split_param)
             image_stat = self.image_stats_cls(dataloader)
             imagestat_results = image_stat.compute_image_stats() 
             setattr(SplitImageStatsResult, split_nm, imagestat_results)
-            print(f"Finished computing image stats for {split_nm} split.")
+            logger.info(f"Finished computing image stats for {split_nm} split.")
         return SplitStatsResult(image_stats=SplitImageStatsResult)
