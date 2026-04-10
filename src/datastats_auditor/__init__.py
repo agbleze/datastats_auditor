bc4c9cfd6a441d234d87abf8920fa8f202f0c5b4
linus <lagbleze@st.ug.edu.gh>
2026-04-10 23:40:42 +0200

    style: remove comments and spaces

--- src/datastats_auditor/__init__.py
+++ src/datastats_auditor/__init__.py
@@ -1,3 +1,14 @@
-# read version from installed package
 from importlib.metadata import version
-__version__ = version("datastats_auditor")
+import logging
+
+package_name = __name__
+__version__ = version(package_name)
+
+logger = logging.getLogger(package_name)
+logging.basicConfig(level=logging.DEBUG, 
+                    format="%(asctime)s - %(levelname)s - %(message)s",
+                    filemode="a",
+                    filename="datastats_logs.logs"
+                    )
+
+logger.info(f"{package_name} version {__version__}")
